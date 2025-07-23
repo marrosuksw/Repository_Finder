aa01a29dbef305146af66e94fbf1d1a0380d4898
marrosuksw <marcelrossi03@gmail.com>
2025-07-24 00:19:56 +0200

    Feat: add RestClientException handler to handle unexpected github api errors

--- src/main/java/pl/repo_finder/handler/GlobalExceptionHandler.java
+++ src/main/java/pl/repo_finder/handler/GlobalExceptionHandler.java
@@ -1,9 +1,10 @@
-package pl.repo_finder;
+package pl.repo_finder.handler;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -11,5 +12,9 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = {HttpClientErrorException.class})
     public ResponseEntity<ErrorResponse> handleUserNotFoundException(HttpClientErrorException exception) {
         return ResponseEntity.status(404).body(new ErrorResponse(404, "User does not exist."));
+    }
+    @ExceptionHandler(value = {RestClientException.class})
+    public ResponseEntity<ErrorResponse> handleRestClientException(RestClientException exception) {
+        return ResponseEntity.status(500).body(new ErrorResponse(500, exception.getMessage()));
     }
 }
